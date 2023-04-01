bd1a11dee11168209a7778f1c2375f36f071956c
Fred Duarte <fduartej@gmail.com>
2023-03-31 21:14:50 -0500

    Formulario Contacto y Su registro

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -5,20 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using appfunko.Models;
+using appfunko.Data;
 
 namespace appfunko.Controllers
 {
     public class ContactoController : Controller
     {
         private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public ContactoController(ILogger<ContactoController> logger)
+        public ContactoController(ILogger<ContactoController> logger,
+         ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registra(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "El contacto ya esta registrado";
             return View();
         }
 
